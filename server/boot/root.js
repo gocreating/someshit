72751846bf1f1f8646e0c966207ed0aaa1015fff
翁治平 <gocreating@gmail.com>
2018-06-20 18:33:20 +0800

    Handle robots.txt

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,5 +4,8 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/status', server.loopback.status());
+  router.get('/robots.txt', function(req, res, next) {
+    res.send('User-agent: *\nDisallow:');
+  });
   server.use(router);
 };
