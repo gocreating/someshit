3acfc00b51e577b2cd62be6883a3aa5688d582e1
翁治平 <gocreating@gmail.com>
2018-06-25 01:05:39 +0800

    Add redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(rootReducer);
+
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'));
 registerServiceWorker();
